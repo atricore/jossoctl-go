ddf945f1de01f6a5a85e5c24658352401a942d08
Sebastian <sgonzalezoyuela@gmail.com>
2022-11-14 13:35:57 -0500

    New iamtfctl command

---
+++ iamtfctl/iamtfctl.go
@@ -0,0 +1,13 @@
+/*
+Copyright © 2022 Sebastian Gonzalez Oyuela sgonzalez@atricore.com
+
+*/
+package main
+
+import (
+	"github.com/atricore/josso-cli-go/cmd"
+)
+
+func main() {
+	cmd.ExecuteIamtf()
+}
