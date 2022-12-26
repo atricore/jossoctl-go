62d53b6d0bda3aa7d3347d68762b24fad50417d1
Sebastian <sgonzalezoyuela@gmail.com>
2022-12-26 18:15:12 -0500

    Centralized extension formatter for customclassdto

---
+++ formatter/extension.go
@@ -0,0 +1,77 @@
+package formatter
+
+import api "github.com/atricore/josso-api-go"
+
+/**
+* Any wrapper that has a CustomClassDTO must define an Extension() method that returns a CustomClassWrapper
+ */
+
+// Insert this snippet into your main template
+const (
+	extensionFormat = `
+	{{- if .Extension.HasExtension }}
+	Extension
+    FCQN:        {{.Extension.FCQN}}
+    Osgi Filter: {{.Extension.OsgiFilter}}
+    Type:        {{.Extension.Type}}
+   
+    Properties {{ range $props := .Extension.CustomClassProperties}}
+                Name:   {{$props.Name}}
+                Value:  {{$props.Value}}
+    {{ end }}{{- end }}
+	{{- if not .Extension.HasExtension }}
+	Extension: not configured
+	{{- end }}
+`
+)
+
+type CustomClassWrapper struct {
+	cc *api.CustomClassDTO
+}
+
+func (c *CustomClassWrapper) HasExtension() bool {
+	val, ok := c.cc.GetFqcnOk()
+	return ok && *val != ""
+}
+
+func (c *CustomClassWrapper) FCQN() string {
+	return c.cc.GetFqcn()
+}
+
+func (c *CustomClassWrapper) OsgiFilter() string {
+	return c.cc.GetOsgiFilter()
+}
+
+func (c *CustomClassWrapper) CustomClassProperties() []CustomClassPropsWrapper {
+
+	var ccpWrappers []CustomClassPropsWrapper
+
+	if c.cc.GetProperties() == nil {
+		return ccpWrappers
+	}
+	ccp := c.cc.Properties
+	for i := range ccp {
+		ccpWrappers = append(ccpWrappers, CustomClassPropsWrapper{props: &c.cc.GetProperties()[i]})
+	}
+	return ccpWrappers
+}
+
+func (c *CustomClassWrapper) Type() string {
+	if c.cc.GetOsgiService() {
+		return "SERVICE"
+	} else {
+		return "INSTANCE"
+	}
+}
+
+type CustomClassPropsWrapper struct {
+	props *api.CustomClassPropertyDTO
+}
+
+func (c *CustomClassProp) Name() string {
+	return c.props.GetName()
+}
+
+func (c *CustomClassProp) Value() string {
+	return c.props.GetValue()
+}
